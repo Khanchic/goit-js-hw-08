f277791211633ac5d7b97276f5a4b80e9a8de463
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2022-11-01 13:48:35 +0200

    finally commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+const trottle = require('lodash.throttle');
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  trottle(function (data) {
+    const videoCurrentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', videoCurrentTime);
+  }, 1000)
+);
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'TypeError':
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import trottle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+UsersInputs();
+formRef.addEventListener(
+  'input',
+  trottle(function (e) {
+    const inputValue = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+
+    const objValue = {
+      email: inputValue,
+      message: message,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(objValue));
+  }),
+  1000
+);
+
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+  const localStorageIs = localStorage.getItem('feedback-form-state');
+  const obj = JSON.parse(localStorageIs);
+  console.log(obj);
+  formRef.reset();
+  localStorage.removeItem('feedback-form-state');
+});
+
+function UsersInputs() {
+  const localStorageIs = localStorage.getItem('feedback-form-state');
+  const objSaves = JSON.parse(localStorageIs);
+
+  if (objSaves) {
+    input.value = objSaves.email;
+    textarea.value = objSaves.message;
+  }
+}
